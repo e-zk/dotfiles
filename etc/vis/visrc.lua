2d207466e76dc1af951edf8cfa04fc0fffd96eba
e-zk <58356365+e-zk@users.noreply.github.com>
2020-07-11 19:00:42 +1000

    hmmm

--- etc/vis/visrc.lua
+++ etc/vis/visrc.lua
@@ -2,30 +2,23 @@
 require('vis')
 
 -- plugins
---require('plugins/vis-go/vis-go')
+require('plugins/vis-go')
 --require('plugins/vis-filetype-settings/vis-filetype-settings')
---require('plugins/vis-shebang/vis-shebang')
+require('plugins/vis-shebang')
 
--- per file settings
-settings = {
-	--markdown = {"set expandtab on", "set tabwidth 4"}
-	bash = {"set syntax bash"}
---	latex = function(win)
---		local this_file = vis.win.file
---		if this_file.size == 0 then
---		end
---	end
-
-}
-
--- shebang syntaxes
-shebangs = {
-	["#!/bin/sh"] = "bash",
-	["#!/bin/ksh"] = "bash"
-}
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
+	-- per file settings
+	settings = {
+		markdown = {"set expandtab on", "set tabwidth 4"}
+	}
+	
+	-- shebang syntax
+	shebangs = {
+		["#!/bin/sh"] = "bash",
+		["#!/bin/ksh"] = "bash"
+	}
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
